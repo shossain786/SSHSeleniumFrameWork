b961653100ee62e7f4813256da01ee5ed308e9ad
shossain786 <sddmhossain786@gmail.com>
2019-01-07 23:41:33 +0530

    added

--- NewJavaProject/src/test/selenium/Test1.java
+++ NewJavaProject/src/test/selenium/Test1.java
@@ -2,7 +2,8 @@ package test.selenium;
 
 public class Test1 {
 	public static void main(String[] args) {
-		String firstName = "Md Saddam";
+		System.out.println("This is  start of the test.");
+		String firstName = "Saddam Banu";
 		String lastName = "Hossain";
 		System.out.println("Hello, "+firstName+" "+lastName);
 	}
