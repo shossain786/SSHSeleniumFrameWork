991e2465b5de567cd1bcf7787f4b472882d5197e
shossain786 <sddmhossain786@gmail.com>
2019-01-08 00:01:24 +0530

    ok

--- NewJavaProject/src/test/selenium/Test1.java
+++ NewJavaProject/src/test/selenium/Test1.java
@@ -3,8 +3,9 @@ package test.selenium;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("This is  start of the test.");
-		String firstName11 = "Saddam Banu";
+		String firstName22 = "Saddam"; int age = 20;
 		String lastName = "Hossain";
-		System.out.println("Hi, "+firstName11+" "+lastName);
+		System.out.println("Hi, "+firstName22+" "+lastName);
+		System.out.println("OOPS! You just crossed teenage. You are of "+ age);
 	}
 }
